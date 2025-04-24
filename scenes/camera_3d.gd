ffa321048d4234a223a7c3445bfdd1f790a1e657
xMagicWarriorx <magic.warrior@o2.pl>
2025-04-24 23:02:10 +0200

    zoom

--- scenes/camera_3d.gd
+++ scenes/camera_3d.gd
@@ -1,7 +1,11 @@
 extends Camera3D
 
-
+var sensitivity = 0.05
 func _input(event):
 	if event is InputEventScreenDrag:
 		var rel = event.relative
-		global_position += Vector3(rel.x, 0, rel.y)
+		global_position += Vector3(rel.x, 0, rel.y)*sensitivity
+	elif event is InputEventMagnifyGesture:
+		var zoom_factor = event.factor
+		var zoom_amount = (1.0 - zoom_factor)
+		global_translate(Vector3(0, 0, zoom_amount))
