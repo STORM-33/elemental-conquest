acb5ae84072ae6f17b551fb1a23314d69250f8db
xMagicWarriorx <magic.warrior@o2.pl>
2025-04-25 17:42:15 +0200

    settings menu

---
+++ globals.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var camera_sensitivity = 0.05
+@onready var screen_size = get_viewport().get_visible_rect().size
+@onready var screen_width = screen_size.x
+@onready var screen_height = screen_size.y
